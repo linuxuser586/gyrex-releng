8fb145c1dbb12509c63b5b366ff78629c0d17f68
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2009-10-12 10:53:42 +0000

    modified script to also run on any machine

--- org.eclipse.gyrex.releng/build/gyrex-1.0.x/scripts/gyrex-1.0.x-nightly.sh
+++ org.eclipse.gyrex.releng/build/gyrex-1.0.x/scripts/gyrex-1.0.x-nightly.sh
@@ -1,21 +1,10 @@
-#*******************************************************************************
-# Copyright (c) 2009 AGETO Service GmbH and others.
-# All rights reserved.
-#  
-# This program and the accompanying materials are made available under the 
-# terms of the Eclipse Public License v1.0 which accompanies this distribution,
-# and is available at http://www.eclipse.org/legal/epl-v10.html.
-#
-# Contributors:
-#     Gunnar Wagenknecht - initial API and implementation
-#*******************************************************************************
 #!/bin/bash
 
 # This script runs as https://build.eclipse.org/hudson/job/cbi-*/configure
 # and is archived in  http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.dash/athena/org.eclipse.dash.commonbuilder/org.eclipse.dash.commonbuilder.releng/hudson/?root=Technology_Project
 # Build runs under ${WORKSPACE} == /opt/users/hudsonbuild/.hudson/jobs/cbi-*/workspace
 
-#echo "[`date +%Y/%m/%d\ %H:%M`] Hudson job ${JOBNAME} build #${BUILD_NUMBER} (${BUILD_ID}) started." 
+echo "[`date +%Y/%m/%d\ %H:%M`] Hudson job ${JOBNAME} build #${BUILD_NUMBER} (${BUILD_ID}) started." 
 
 ##############################################################################################
 
@@ -28,6 +17,24 @@ version="1.0.0"
 # leave blank to use assumed values (which may be wrong!)
 projRelengRoot='-projRelengRoot :pserver:anonymous@dev.eclipse.org:/cvsroot/technology'
 projRelengPath='-projRelengPath org.eclipse.gyrex/releng/org.eclipse.gyrex.releng/build/gyrex-1.0.x'
+
+# where should we look for pre-checked out project sources for org.eclipse.dash.common.releng and org.eclipse.releng.basebuilder ?
+cvsProjectBaseDir=/opt/public/cbi/build # build.eclipse.org
+if [[ ! -d $cvsProjectBaseDir ]]; then cvsProjectBaseDir=~/buildworkspace; fi # local build? 
+if [[ ! -d $cvsProjectBaseDir ]]; then 
+	echo "ERROR: cannot find where org.eclipse.dash.common.releng and org.eclipse.releng.basebuilder are on disk. Must exit!"
+	exit 1;
+fi 
+
+# need a place to store existing 3rd party jars, eg., ant-contrib.jar (if not in /usr/share/java/)
+thirdPartyJarsDir=/opt/public/cbi/build/3rdPartyJars # build.eclipse.org
+if [[ ! -d $thirdPartyJarsDir ]]; then thirdPartyJarsDir=/tmp/build/3rdPartyJars; fi # local build 
+if [[ ! -d $thirdPartyJarsDir ]]; then mkdir $thirdPartyJarsDir; fi 
+
+# define where to do all the work; start with a fresh folder each time
+writableBuildRoot="${WORKSPACE}/build"
+if [[ ! -d $writableBuildRoot ]]; then writableBuildRoot=~/buildworkspace; fi # local build? 
+if [[ -d ${writableBuildRoot} ]]; then rm -fr ${writableBuildRoot}; fi
 
 # DONE CONFIGURATION
 
@@ -44,10 +51,6 @@ fi
 # buildAlias will rename zips from foo-SDK-N200901011234.zip to foo-SDK-1.0.0RC2.zip
 
 ##############################################################################################
-
-# define where to do all the work; start with a fresh folder each time
-writableBuildRoot="/tmp/gyrexbuild"
-if [[ -d ${writableBuildRoot} ]]; then rm -fr ${writableBuildRoot}; fi
 
 # define required folders
 downloadsDir="${writableBuildRoot}/downloads"
@@ -68,14 +71,19 @@ touch ${writableBuildRoot}/.cvspass
 relengBaseBuilderDir="${writableBuildRoot}/org.eclipse.releng.basebuilder"
 relengCommonBuilderDir="${writableBuildRoot}/org.eclipse.dash.common.releng"
 # symlink basebuilder and common.releng; alternatively, if you omit this, they'll be checked out in start.sh
-ln -s /opt/public/cbi/build/org.eclipse.releng.basebuilder ${writableBuildRoot}/
-ln -s /opt/public/cbi/build/org.eclipse.dash.common.releng ${writableBuildRoot}/
+ln -s ${cvsProjectBaseDir}/org.eclipse.releng.basebuilder ${writableBuildRoot}/
+ln -s ${cvsProjectBaseDir}/org.eclipse.dash.common.releng ${writableBuildRoot}/
+
+#symlink local copy of org.eclipse.gef.releng instead of fetching from CVS (so tweaks for local Hudson are preserved)
+if [[ -d ${cvsProjectBaseDir}/org.eclipse.gef.releng ]]; then
+	ln -s ${cvsProjectBaseDir}/org.eclipse.gef.releng ${writableBuildRoot}/
+fi
 
 # symlink 3rdPartyJars (reuse existing content on build.eclipse.org for ant-contrib.jar, etc.)
+ln -s ${thirdPartyJarsDir} ${writableBuildRoot}/
 thirdPartyJarsDir="${writableBuildRoot}/3rdPartyJars"
-ln -s /opt/public/cbi/build/3rdPartyJars ${writableBuildRoot}/
 
-# run a build
+# run a build - may have to pass in "-javaHome /usr/lib/jvm/java" or similar here if default JVM not found
 cd ${writableBuildRoot}/org.eclipse.dash.common.releng/tools/scripts
 ./start.sh -projectid ${projectid} -version ${version} -buildType ${buildType} -buildTimestamp ${buildTimestamp} \
   -writableBuildRoot ${writableBuildRoot} -thirdPartyJarsDir ${thirdPartyJarsDir} -downloadsDir ${downloadsDir} -buildDir ${buildDir} \
@@ -84,4 +92,4 @@ cd ${writableBuildRoot}/org.eclipse.dash.common.releng/tools/scripts
 # remove file so workspace navigation is one click simpler
 rm -f ${writableBuildRoot}/.cvspass
 
-#echo "[`date +%Y/%m/%d\ %H:%M`] Hudson job ${JOBNAME} build #${BUILD_NUMBER} (${BUILD_ID}) done." 
+echo "[`date +%Y/%m/%d\ %H:%M`] Hudson job ${JOBNAME} build #${BUILD_NUMBER} (${BUILD_ID}) done." 
