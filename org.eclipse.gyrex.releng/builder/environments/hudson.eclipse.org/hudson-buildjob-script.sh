a75a0e177db3adc0d5e7cc1218c90d3cea287931
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2011-02-14 13:22:53 +0000

    started work on publishing builds automatically

--- org.eclipse.gyrex.releng/builder/environments/hudson.eclipse.org/hudson-buildjob-script.sh
+++ org.eclipse.gyrex.releng/builder/environments/hudson.eclipse.org/hudson-buildjob-script.sh
@@ -26,6 +26,13 @@ if [ "$BUILD_TYPE" = "N" ]; then
 elif [ "$BUILD_TYPE" = "I" ]; then
     echo '# customization for integration builds' >> $buildLocalProps
     echo skipSign=true >> $buildLocalProps
+	echo publishRepoStream=1.0/integration >> $buildLocalProps
+elif [ "$BUILD_TYPE" = "M" ]; then
+    echo '# customization for maintenance builds' >> $buildLocalProps
+	echo publishRepoStream=1.0/maintenance >> $buildLocalProps
+elif [ "$BUILD_TYPE" = "S" ]; then
+    echo '# customization for stable builds' >> $buildLocalProps
+	echo publishRepoStream=1.0/milestones >> $buildLocalProps
 fi
 echo '#' >> $buildLocalProps
 echo "# generated `date`" >> $buildLocalProps
