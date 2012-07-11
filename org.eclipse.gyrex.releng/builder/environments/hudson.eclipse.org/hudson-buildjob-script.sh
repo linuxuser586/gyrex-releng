d1dbcbd471c69df78a01482245596277168f29f0
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-07-11 10:28:41 +0200

    new development iteration
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- org.eclipse.gyrex.releng/builder/environments/hudson.eclipse.org/hudson-buildjob-script.sh
+++ org.eclipse.gyrex.releng/builder/environments/hudson.eclipse.org/hudson-buildjob-script.sh
@@ -46,16 +46,16 @@ if [ "$BUILD_TYPE" = "N" ]; then
 elif [ "$BUILD_TYPE" = "I" ]; then
 	echo '# customization for integration builds' >> $buildLocalProps
 	echo skipSign=true >> $buildLocalProps
-	echo publishRepoStream=1.1/integration >> $buildLocalProps
+	echo publishRepoStream=1.2/integration >> $buildLocalProps
 elif [ "$BUILD_TYPE" = "M" ]; then
 	echo '# customization for maintenance builds' >> $buildLocalProps
-	echo publishRepoStream=1.0/maintenance >> $buildLocalProps
+	echo publishRepoStream=1.1/maintenance >> $buildLocalProps
 elif [ "$BUILD_TYPE" = "S" ]; then
 	echo '# customization for stable builds' >> $buildLocalProps
-	echo publishRepoStream=1.1/milestones >> $buildLocalProps
+	echo publishRepoStream=1.2/milestones >> $buildLocalProps
 elif [ "$BUILD_TYPE" = "R" ]; then
 	echo '# customization for release builds' >> $buildLocalProps
-	echo publishRepoStream=1.0 >> $buildLocalProps
+	echo publishRepoStream=1.1 >> $buildLocalProps
 fi
 echo '' >> $buildLocalProps
 echo "# generated `date`" >> $buildLocalProps
